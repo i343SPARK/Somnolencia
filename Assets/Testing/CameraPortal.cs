132472d8de45c61c72cadf6ea86bb2f0392c65f6
Pablo Salas <pablosalas_@outlook.com>
2023-03-15 17:11:17 -0600

    SomethingBadass

--- Assets/Testing/CameraPortal.cs
+++ Assets/Testing/CameraPortal.cs
@@ -18,11 +18,10 @@ public class CameraPortal : MonoBehaviour
     {
         //imita la rotacion de la camara y gira 180 grados para que el portal se vea bien
         transform.rotation = Quaternion.Euler(otherCam.transform.rotation.eulerAngles.x,
-            otherCam.transform.rotation.eulerAngles.y + 180, 270);
-        
-        //imita la posicion de la camara
-        transform.position = -otherCam.transform.position;
-        
-        
+            otherCam.transform.rotation.eulerAngles.y + 180, 0);
+
+        Vector3 pos = transform.InverseTransformPoint(otherCam.transform.position);
+
+        transform.position = new Vector3(pos.x, -pos.y, pos.z);
     }
 }

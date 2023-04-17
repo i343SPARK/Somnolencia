bd04281cb63ea9a4688163fc3fc2de57b5d5faa9
Pablo Salas <pablosalas_@outlook.com>
2023-04-17 00:37:07 -0600

    Pedro

---
+++ Assets/Scripts/WindowScriptEvent.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowScriptEvent : MonoBehaviour
+{
+    public GameObject _UIKeyInteractive, SoundProgresive;
+    public Animator _animator;
+    
+    bool secure = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (secure && Input.GetKeyDown(KeyCode.E))
+        {
+            _UIKeyInteractive.GetComponent<ShowUIKeyInteractive>().Interact();
+            _animator.Play("WindowEvent");
+            SoundProgresive.SetActive(true);
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _UIKeyInteractive.SetActive(true);
+            secure = true;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _UIKeyInteractive.SetActive(false);
+            secure = false;
+        }
+    }
+}

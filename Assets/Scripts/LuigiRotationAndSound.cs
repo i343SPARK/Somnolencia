a697cabca4223259db30c4ad688fe9d3311c7668
Pablo Salas <pablosalas_@outlook.com>
2023-04-20 13:22:38 -0600

    sadee

---
+++ Assets/Scripts/LuigiRotationAndSound.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuigiRotationAndSound : MonoBehaviour
+{
+    bool secure = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (secure && transform.rotation.z > -0.83f)
+        {
+            transform.Rotate(0, 0, -120 * Time.deltaTime);
+            Debug.Log(transform.rotation.z);
+        }
+
+        if (transform.rotation.z < -0.8f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            secure = true;
+        }
+    }
+}
--- Assets/Scripts/TestingFunctionsScript.cs
+++ Assets/Scripts/TestingFunctionsScript.cs
@@ -6,19 +6,16 @@ using UnityEngine;
 public class TestingFunctionsScript : MonoBehaviour
 {
     // luces de la escena, sonido de las luces, musica de ambiente, sonido de prueba, pantalla de fade
-    public GameObject lights, sound, sound2, soundTest, fadeScreen;
-    // variable para activar y desactivar las luces
-    private bool toggleLights = true;
-    // variable para asegurar que el sonido se desactive hasta que el pitch llegue a cero
-    private bool secureSound = false;
+    //public GameObject lights, sound, sound2, soundTest, fadeScreen;
+    public GameObject player, player2, soundAmbient;
     
     
     // Start is called before the first frame update
     void Start()
     { 
         // Se asegura que se desactivan las luces y que se activa la pantalla de fade
-        soundTest.SetActive(false);
-        fadeScreen.SetActive(true);
+        //soundTest.SetActive(false);
+        //fadeScreen.SetActive(true);
       
     }
 
@@ -27,42 +24,10 @@ public class TestingFunctionsScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.J))
         {
-            // si toggleLights es true, desactiva las luces y activa el sonido
-            if (toggleLights)
-            {
-                lights.SetActive(false);
-                sound.SetActive(true);
-                RenderSettings.fog = true;
-                secureSound = true;
-                toggleLights = false;
-            }
-            // si toggleLights es false, activa las luces y desactiva el sonido
-            else
-            {
-                secureSound = false;
-                lights.SetActive(true);
-                sound.SetActive(false);
-                RenderSettings.fog = false;
-                sound2.GetComponent<AudioSource>().pitch = 1;
-                toggleLights = true;
-            }
-
-        }
-        
-        // Reduce el pitch del sonido2(musica de ambiente) de forma lineal hasta llegar a cero
-        if (secureSound && sound2.GetComponent<AudioSource>().pitch > -0.1f)
-        {
-            sound2.GetComponent<AudioSource>().pitch -= 0.1f * Time.deltaTime;
-        }
-        else
-        {
-            secureSound = false;
-        }
-
-        if (sound2.GetComponent<AudioSource>().pitch < -0.1f)
-        {
-            soundTest.SetActive(true);
-            sound2.SetActive(false);
+            player.SetActive(false);
+            player2.SetActive(true);
+            soundAmbient.SetActive(false);
+            RenderSettings.fog = true;
         }
     }
     

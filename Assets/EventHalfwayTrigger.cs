86367d2f1ee70e60bda18ddfcdb622c8577ee232
Pablo Salas <pablosalas_@outlook.com>
2023-04-23 23:40:08 -0600

    Sustos que dan gusto

---
+++ Assets/EventHalfwayTrigger.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHalfwayTrigger : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(("Player")))
+        {
+            anim.Play("DoorAnimation");
+            RenderSettings.fog = true;
+            Destroy(gameObject);
+        }
+    }
+}

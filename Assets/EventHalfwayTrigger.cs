ca03fdc2100f3f148ebb24889e62abf350bf926a
Pablo Salas <pablosalas_@outlook.com>
2023-04-25 10:53:22 -0600

    Minecraft Storiboard

--- Assets/EventHalfwayTrigger.cs
+++ Assets/EventHalfwayTrigger.cs
@@ -6,15 +6,32 @@ using UnityEngine;
 public class EventHalfwayTrigger : MonoBehaviour
 {
     public Animator anim;
+
+    public float desinty = 0;
+    bool secure = false;
     // Start is called before the first frame update
+
+    private void Update()
+    {
+        if (RenderSettings.fogDensity < 0.46f && secure)
+        {
+            RenderSettings.fogDensity += desinty * Time.deltaTime;
+            Debug.Log(RenderSettings.fogDensity);
+        }
+
+        if (RenderSettings.fogDensity >= 0.46f)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(("Player")))
         {
             anim.Play("DoorAnimation");
-            RenderSettings.fog = true;
-            Destroy(gameObject);
+            secure = true;
+            // RenderSettings.fog = true;
         }
     }
 }
